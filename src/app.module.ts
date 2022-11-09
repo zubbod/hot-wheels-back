c200a0eac5a5a6c68532f5c68d49eada9845f083
Zubashenko Bogdan <zubbod@hotmail.com>
2022-11-09 19:31:39 +0300

    change port

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import { UserModule } from './modules/user/user.module';
       host: process.env.POSTGRES_HOST,
       port: Number(process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_USER_PASSWORD,
+      password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
       models: [UserModel, RoleModel, UserRolesModel, CarModel],
       autoLoadModels: true,
