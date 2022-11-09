b5f8cfa0e67ef87da23ee5ebea860ed8f7128119
Zubashenko Bogdan <zubbod@hotmail.com>
2022-11-09 19:56:42 +0300

    change port

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { resolve } from 'path';
+import { Dialect } from 'sequelize';
 import { CarModel } from 'src/models/car.model';
 import { RoleModel } from 'src/models/role.model';
 import { UserModel } from 'src/models/user.model';
@@ -13,21 +14,34 @@ import { AuthModule } from './modules/auth/auth.module';
 import { RoleModule } from './modules/role/role.module';
 import { UserModule } from './modules/user/user.module';
 
+// export const SEQUELIZE_CONF = {
+//   dialect: 'postgres' as Dialect,
+//   host: process.env.POSTGRES_HOST,
+//   port: Number(process.env.POSTGRES_PORT),
+//   username: process.env.POSTGRES_USER,
+//   password: process.env.POSTGRES_PASSWORD,
+//   database: process.env.POSTGRES_DATABASE,
+//   models: [UserModel, RoleModel, UserRolesModel, CarModel],
+//   autoLoadModels: true,
+// };
+
+export const SEQUELIZE_CONF = {
+  dialect: 'postgres' as Dialect,
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '1902',
+  database: 'postgres',
+  models: [UserModel, RoleModel, UserRolesModel, CarModel],
+  autoLoadModels: true,
+};
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      models: [UserModel, RoleModel, UserRolesModel, CarModel],
-      autoLoadModels: true,
-    }),
+    SequelizeModule.forRoot(SEQUELIZE_CONF),
     UserModule,
     RoleModule,
     AuthModule,
@@ -44,4 +58,8 @@ import { UserModule } from './modules/user/user.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(SEQUELIZE_CONF);
+  }
+}
